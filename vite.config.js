258434d1404e589f7e2d19fe58c3ea9b54c2bbff
ishan-web-dev <joshiishaan86@gmail.com>
2025-07-19 14:25:59 +0530

    fix live function

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: 'Ishan.J-Website', // 🔁 Replace with your GitHub repo name
-  plugins: [react(),
-    tailwindcss(),
-  ],
-    server: {
-    host: true, // ← This line makes Vite accessible on the local network
-  }
-})
+  base: process.env.NODE_ENV === 'production' ? '/Ishan.J-Website/' : '/',
+  plugins: [react(), tailwindcss()],
+  server: {
+    // port: 5173, np// optional: set port
+    // open: true, // optional: auto-open browser
+  },
+});
